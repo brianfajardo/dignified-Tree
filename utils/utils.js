ba1fe912fed49d0398f42ad9f877b73c83965526
Brian Fajardo <bmf5@hotmail.com>
2017-03-07 05:39:11 -0500

    expect package

--- utils/utils.js
+++ utils/utils.js
@@ -2,7 +2,15 @@ const add = (a, b) => a + b;
 
 const square = (x) => x * x;
 
+const setName = (userObj, fullName) => {
+    const names = fullName.split(' ');
+    userObj.firstName = names[0];
+    userObj.lastName = names[1]
+    return userObj
+}
+
 module.exports = {
     add,
-    square
+    square,
+    setName
 }
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,19 +1,47 @@
 // Behaviour driven development (Mocha)
 const utils = require('./utils');
+const expect = require('expect');
 
 it('should add two numbers', () => {
     const res = utils.add(5, 10);
 
-    if (res !== 15) {
-        throw new Error(`Expected: 15, but got ${res}.`)
-    };
-
+    expect(res)
+        .toBe(15)
+        .toBeA('number')
 });
 
 it('should square a number', () => {
     const res = utils.square(5);
 
-    if (res !== 25) {
-        throw new Error(`Expected: 25, but got ${res}.`)
+    expect(res)
+        .toBe(25)
+        .toBeA('number')
+});
+
+it('should split full name into user.firstName and user.lastName', () => {
+    const user = {
+        location: 'Sweden',
+        occupation: 'producer'
     };
-})
+    const res = utils.setName(user, 'Eric Prydz');
+
+    expect(res)
+        .toBeA('object')
+        .toInclude({
+            firstName: 'Eric',
+            lastName: 'Prydz'
+        })
+});
+
+// it('should expect a value', () => {
+//     // expect(1).toNotBe(2)
+//     // expect({color: 'orange'}).toNotEqual({color: 'red'})
+//     // expect([1,2,3]).toInclude(2)
+//     // expect([1,2,3]).toExclude(4)
+//     // expect({
+//     //     name:'Brian',
+//     //     location:'Canada',
+//     //     season: 'spring'
+//     // }).toInclude({
+//     //     location:'Canada'
+// })
