aa2baa3fd9b4dbae4be64961506497392b1ed1a5
Brian Fajardo <bmf5@hotmail.com>
2017-03-08 02:15:07 -0500

    organize tests with mocha describe()

--- server/server.test.js
+++ server/server.test.js
@@ -3,34 +3,39 @@ const expect = require('expect')
 
 const app = require('./server').app
 
-it('should return the object response', (done) => {
-    request(app) /* supertest */
-        .get('/')
-        .expect(404)
-        .expect((response) => {
-            expect(response.body) /* expect dev dependency, not request's */
-                .toInclude({
-                    error: 'Page not found'
-                })
-        }) 
-        .end(done) /* tells mocha and supertest end of test */
-})
+describe('Server', () => {
 
-// mocha framework testcase
-// assert status code is 200
-// assert that object with name property 'Brian' exists in users array
-
-it('should return the users array response', (done) => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body)
-                .toBeA('array')
-                .toInclude({
-                    name: 'Brian',
-                    age: 23
+    describe('GET /', () => {
+        it('should return the object response', (done) => {
+            request(app) /* supertest */
+                .get('/')
+                .expect(404)
+                .expect((response) => {
+                    expect(response.body) /* expect dev dependency, not request's */
+                        .toInclude({
+                            error: 'Page not found'
+                        })
                 })
+                .end(done) /* tells mocha and supertest end of test */
         })
-        .end(done)
+    })
+
+    describe('GET /users', () => {
+        // assert status code is 200
+        // assert that object with name property 'Brian' exists in users array
+        it('should return the users array response', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body)
+                        .toBeA('array')
+                        .toInclude({
+                            name: 'Brian',
+                            age: 23
+                        })
+                })
+                .end(done)
+        })
+    })
 })
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,22 +1,47 @@
 // Behaviour driven development (Mocha)
-const utils = require('./utils');
 const expect = require('expect');
 
-it('should add two numbers', () => {
-    const res = utils.add(5, 10);
+const utils = require('./utils');
 
-    expect(res)
-        .toBe(15)
-        .toBeA('number')
-});
+describe('Utils', () => { /* describe() is a Mocha function for grouping test cases (organization) */
 
-it('should square a number', () => {
-    const res = utils.square(5);
+    it('should add two numbers', () => {
+        const res = utils.add(5, 10);
 
-    expect(res)
-        .toBe(25)
-        .toBeA('number')
-});
+        expect(res)
+            .toBe(15)
+            .toBeA('number')
+    });
+
+    it('should square a number', () => {
+        const res = utils.square(5);
+
+        expect(res)
+            .toBe(25)
+            .toBeA('number')
+    });
+
+    describe('#asynchronous', () => {
+        // passing 'done' tells Mocha that the test is not done yet (ie. setTimeout example)
+        it('should async add two numbers after 1 second', (done) => {
+            utils.asyncAdd(2, 3, (sum) => {
+                expect(sum)
+                    .toBe(5)
+                    .toBeA('number');
+                done() /* tells Mocha test is done and can process result for pass/fail */
+            })
+        });
+
+        it('should async square two numbers after 1 second', (done) => {
+            utils.asyncSquare(10, (squared) => {
+                expect(squared)
+                    .toBe(100)
+                    .toBeA('number');
+                done()
+            })
+        });
+    })
+})
 
 it('should split full name into user.firstName and user.lastName', () => {
     const user = {
@@ -31,23 +56,4 @@ it('should split full name into user.firstName and user.lastName', () => {
             firstName: 'Eric',
             lastName: 'Prydz'
         })
-});
-
-// passing 'done' tells Mocha that the test is not done yet (ie. setTimeout example)
-it('should async add two numbers after 1 second', (done) => {
-    utils.asyncAdd(2, 3, (sum) => {
-        expect(sum)
-            .toBe(5)
-            .toBeA('number');
-        done() /* tells Mocha test is done and can process result for pass/fail */
-    })
-});
-
-it('should async square two numbers after 1 second', (done) => {
-    utils.asyncSquare(10, (squared) => {
-        expect(squared)
-            .toBe(100)
-            .toBeA('number');
-        done()
-    })
 });
